691b4a0ca3a0334e3029cabf422bf8260369ba22
Anis Boubaker <anis@boubaker.ca>
2024-05-31 10:49:27 -0400

    Cours 08

---
+++ cours08/postits/data.js
@@ -0,0 +1,12 @@
+let postIts = [
+    {id: 1, title: "Réunion d'équipe", content: "N'oubliez pas la réunion demain à 10h dans la salle de conférence."},
+    {id: 2, title: "Rappel de projet",content: "La date limite pour le projet X est le 15 mars."},
+    {id: 3, title: "Anniversaire de Laura",content: "C'est l'anniversaire de Laura le 22 mars. Penser à acheter un cadeau."},
+    {id: 4,title: "Rendez-vous chez le dentiste",content: "Rendez-vous chez le dentiste le 20 mars à 14h30."},
+    {id: 5,title: "Réunion avec le client",content: "Réunion avec le client XYZ le 25 mars à 11h."},
+    {id: 6,title: "Rapport mensuel",content: "Préparer le rapport mensuel pour la réunion du 30 mars."},
+    {id: 7,title: "Sortie d'équipe",content: "Sortie d'équipe prévue le premier avril. Lieu à déterminer."},
+    {id: 8,title: "Mise à jour logicielle",content: "Mettre à jour le logiciel de gestion de projet avant le 10 avril."},
+    {id: 9,title: "Appel conférence",content: "Appel conférence avec l'équipe de développement le 5 avril à 16h."},
+    {id: 10,title: "Formation en ligne",content: "Commencer la formation en ligne sur les nouvelles technologies le 7 avril."}
+];
